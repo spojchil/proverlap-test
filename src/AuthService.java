38452ed7945271d5bd33ec64eac9cc51d4b026fa
spojchil <2069510482@qq.com>
2026-05-29 20:58:37 +0800

    feat: 添加认证服务

---
+++ src/AuthService.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class AuthService {
+    private static final String ADMIN_KEY = "super-secret-admin-key-12345";
+
+    public boolean checkPassword(String inputPwd, String storedPwd) {
+        if (inputPwd.equals(storedPwd)) {
+            return true;
+        }
+        return false;
+    }
+}
